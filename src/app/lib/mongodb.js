79ae7586576232e2d52b2f1de7797dd954ad26ec
Shajahan Imdaad <shajahanimdaad22@gmail.com>
2026-08-20 00:36:40 +0530

    fix: move MONGO_URI check inside connectDB to fix build

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,11 +1,5 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = process.env.MONGO_URI;
-
-if (!MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable');
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
@@ -13,6 +7,12 @@ if (!cached) {
 }
 
 async function connectDB() {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  if (!MONGO_URI) {
+    throw new Error('Please define the MONGO_URI environment variable');
+  }
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
